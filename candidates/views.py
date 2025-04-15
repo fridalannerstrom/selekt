0e53ac2a260d19ea91889737d92dba163b50839b
fridalannerstrom <frida.lannerstrom@tqnordic.com>
2025-04-15 20:33:08 +0200

    feat: bootstrap template with base pages and css/js

--- candidates/views.py
+++ candidates/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Candidate
 
 # Create your views here.
+def candidate_list(request):
+    candidates = Candidate.objects.all().order_by('-uploaded_at')
+    return render(request, 'candidate_list.html', {'candidates': candidates})
+
+def index(request):
+    return render(request, 'index.html')
--- cleoproject/urls.py
+++ cleoproject/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from candidates import views as candidate_views
 
 urlpatterns = [
+    path('', candidate_views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('candidates/', include('candidates.urls')),
 ]
