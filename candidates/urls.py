79b342ae6f1fbe50c50c46d4e8abc8a3daf15eb0
fridalannerstrom <frida.lannerstrom@tqnordic.com>
2025-04-19 14:13:00 +0200

    feat: logout button that redirects to index

--- candidates/urls.py
+++ candidates/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 from candidates import views as candidate_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.candidate_list, name='candidate_list'),
     path('add/', candidate_views.add_candidate, name='add_candidate'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
