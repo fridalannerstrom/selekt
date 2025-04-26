4184c1d32ee6bfabcbf97c5be1b4a5c61f927471
fridalannerstrom <frida.lannerstrom@tqnordic.com>
2025-04-26 13:17:55 +0200

    feat: candidate profile picture upload

--- cleoproject/urls.py
+++ cleoproject/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from candidates import views as candidate_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
     path('dashboard/', candidate_views.dashboard, name='dashboard'),
     path('candidates/', include('candidates.urls')), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
