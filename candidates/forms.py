c1cff19a1ab8c40a210e7abac29f374b94ce4bdc
fridalannerstrom <frida.lannerstrom@tqnordic.com>
2025-04-26 19:30:52 +0200

    feat: new layout and cleaner code for edit and add candidate

--- candidates/forms.py
+++ candidates/forms.py
@@ -4,4 +4,20 @@ from .models import Candidate
 class CandidateForm(forms.ModelForm):
     class Meta:
         model = Candidate
-        fields = ['name', 'email', 'top_skills', 'notes']
+        fields = [
+            'name', 'email', 'phone_number', 'job_title',
+            'profile_summary', 'work_experience', 'education',
+            'location', 'links', 'profile_image', 'other', 'notes', 'top_skills'
+        ]
+        widgets = {
+            'profile_summary': forms.Textarea(attrs={'rows': 4}),
+            'work_experience': forms.Textarea(attrs={'rows': 4}),
+            'education': forms.Textarea(attrs={'rows': 4}),
+            'other': forms.Textarea(attrs={'rows': 4}),
+            'notes': forms.Textarea(attrs={'rows': 4}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
