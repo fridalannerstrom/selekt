9403f4190875eaea503903103fff991fcc055c4f
fridalannerstrom <frida.lannerstrom@tqnordic.com>
2025-05-07 10:09:19 +0200

    refactor: comments in main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,14 @@
+// Wait until the entire DOM is fully loaded
 document.addEventListener('DOMContentLoaded', () => {
+
+    // Initialize the upload script
     initUploadScript();
-  
+
     setTimeout(() => {
-      const alert = document.querySelector('.alert');
-      if (alert) alert.classList.remove('show');
+        // Find the first element with the class "alert" (if any)
+        const alert = document.querySelector('.alert');
+
+        // If an alert exists, remove the "show" class to hide it
+        if (alert) alert.classList.remove('show');
     }, 3000);
-  });
-  
+});
