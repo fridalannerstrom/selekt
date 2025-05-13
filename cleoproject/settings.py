c1790cd2ed83299cf49f42ca59ab815376af0c9d
fridalannerstrom <frida.lannerstrom@tqnordic.com>
2025-05-13 18:37:55 +0200

    fix: environment variables are loaded correctly

--- cleoproject/settings.py
+++ cleoproject/settings.py
@@ -14,8 +14,16 @@ from pathlib import Path
 import os
 import dj_database_url
 
+from dotenv import load_dotenv
+
+load_dotenv()  # For .env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# For local env 
+if os.path.exists(os.path.join(BASE_DIR, "env.py")):
+    import env
 
 
 # Quick-start development settings - unsuitable for production
