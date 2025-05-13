8b2780339334595011031a0e37309e680dcf45de
fridalannerstrom <frida.lannerstrom@tqnordic.com>
2025-05-13 18:42:25 +0200

    refactor: formatting and fallback for settings.py

--- cleoproject/settings.py
+++ cleoproject/settings.py
@@ -10,34 +10,32 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
 import os
-import dj_database_url
+from pathlib import Path
 
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()  # For .env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# For local env 
+# Load local env.py if present (useful in development, e.g. on Windows)
 if os.path.exists(os.path.join(BASE_DIR, "env.py")):
     import env
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-key-for-local")
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'selekt-3ceec5fdccb6.herokuapp.com'] 
-
 
 # Application definition
 
