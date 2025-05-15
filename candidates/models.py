7437c75a95468c324bff8d925ea90f13ee9a2870
fridalannerstrom <frida.lannerstrom@tqnordic.com>
2025-05-15 11:10:43 +0200

    feat: change profile picture to cloudinary uplod

--- candidates/models.py
+++ candidates/models.py
@@ -78,8 +78,7 @@ class CandidateFile(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_image = models.ImageField(
-        upload_to='profile_images/', blank=True, null=True)
+    profile_image = CloudinaryField('image', blank=True, null=True)
     is_first_login = models.BooleanField(default=True)
 
     def __str__(self):
